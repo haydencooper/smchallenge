2d8a9c8687c0b71d11ec92070d27d5987cf03c3e
Hayden Cooper <hydnwc@gmail.com>
2024-03-08 12:52:30 +0800

    added update issue function

--- client.js
+++ client.js
@@ -8,7 +8,7 @@ async function readIssues() {
     const data = await response.json(); // extract json from response
     console.log('All issues: ' + JSON.stringify(data)); // log all issues
 }
-// create issue function
+// create issue function (arrow function just to see if it works for fun)
 const createIssue = async (issue) => { // async function to handle fetch request
     const response = await fetch(url, { // fetch request to server
         method: 'POST',
@@ -20,7 +20,18 @@ const createIssue = async (issue) => { // async function to handle fetch request
     const data = await response.json(); // extract json from response
     console.log('issue(s) created: ' + JSON.stringify(data)); // log created issue
 };
-
+// update issue function
+async function updateIssue(id, issue) {
+    const response = await fetch(`${url}/${id}`, { // fetch request to server with issue id
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(issue), // stringify issue object
+    });
+    const data = await response.json(); // extract json from response
+    console.log('issue updated: ' + JSON.stringify(data)); // log updated issue
+}
 // delete issue function
 async function deleteIssue(id) {
     const response = await fetch(`${url}/${id}`, { // fetch request to server with issue id
@@ -33,10 +44,12 @@ async function deleteIssue(id) {
     }
 }
 
-// tests
+// practical tests
+readIssues();
 createIssue({
     id: 1, title: 'test issue 1', description: 'this is the first test issue',
-    id: 2, title: 'test issue 2', description: 'this is the second issue'
 });
-readIssues();
-deleteIssue(2);
+updateIssue(1, {
+    id: 1, title: 'updated issue', description: 'issue updated'
+});
+deleteIssue(1);
